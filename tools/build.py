33e9df4fb5bcb67441a9d29c27cb24beb08e5047
Naoaki Okazaki <okazaki@c.titech.ac.jp>
2021-12-10 01:23:10 +0900

    Added links to SageMaker Studio Lab; fixed an incorrect Japanese translation.

--- tools/build.py
+++ tools/build.py
@@ -15,16 +15,51 @@ twitter_card = """
     <!-- Google Analytics -->
 """
 
+sagemaker_studio_lab = """
+                    alt="Interact on Colab">Colab</button></a>
+
+        <a class="colab-button" href="https://studiolab.sagemaker.aws/import/github/chokkan/mlnote/blob/master/{}"><button type="button" class="btn btn-secondary topbarbtn"
+            title="起動 SageMaker Studio Lab" data-toggle="tooltip" data-placement="left"><i
+                class="fab fa-aws"></i> SageMaker</button></a>
+"""
+
 def build():
     os.system('jupyter-book build --all .')
     shutil.copyfile('./regression/sgd.mp4', './_build/html/sgd.mp4')
 
-def add_twitter_card():
+def modify_html():
+    # Copy the Twitter card.
     shutil.copyfile('./material/mlnote.png', './_build/html/_static/mlnote.png')
-    for src in glob.glob('_build/html/*.html'):
+    
+    # Modify the generated HTML files.
+    for src in glob.glob('_build/html/**/*.html', recursive=True):
+        print(f'Updating: {src}')
+        
+        # Load the HTML content.
         with open(src) as fi:
             content = fi.read()
+            
+        # Find the path to .ipynb
+        path = ''
+        m = re.search(r'"https://colab\.research\.google\.com/github/chokkan/mlnote/blob/master/([^"]+)"', content)
+        if m is not None:
+            path = m.group(1)
+            print(f'    path: {path}')
+        
+        # Add meta tags for Twitter card.
         content = content.replace('<!-- Google Analytics -->', twitter_card)
+        
+        # An ad-hoc fix for the incorrect translation.
+        content = content.replace('title="発売 ', 'title="起動 ')
+        
+        # Add the button for SageMaker Studio Lab.
+        if path:
+            content = content.replace(
+                'alt="Interact on Colab">Colab</button></a>',
+                sagemaker_studio_lab.format(path)
+                )
+        
+        # Write out the HTML content.
         with open(src, 'w') as fo:
             fo.write(content)
 
@@ -38,4 +73,4 @@ def update_license():
 if __name__ == '__main__':
     #update_license()
     build()
-    add_twitter_card()
+    modify_html()

3bc380584fca970557ce14f1e33148d1e1797570
Naoaki Okazaki <okazaki@c.titech.ac.jp>
2024-12-13 13:57:05 +0900

    Update documentation

--- _static/language_data.js
+++ _static/language_data.js
@@ -5,7 +5,7 @@
  * This script contains the language-specific data used by searchtools.js,
  * namely the list of stopwords, stemmer, scorer and splitter.
  *
- * :copyright: Copyright 2007-2022 by the Sphinx team, see AUTHORS.
+ * :copyright: Copyright 2007-2024 by the Sphinx team, see AUTHORS.
  * :license: BSD, see LICENSE for details.
  *
  */
@@ -13,7 +13,7 @@
 var stopwords = [];
 
 
-/* Non-minified version is copied as a separate JS file, is available */
+/* Non-minified version is copied as a separate JS file, if available */
 
 /**
  * Dummy stemmer for languages without stemming rules.
